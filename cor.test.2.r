f1fe8cdc9a696cb9c65013e64232a8b93dbbdefa
MatsushitaT <MatsushitaT@neurology.ucsf.edu>
2012-07-25 12:22:10 -0700

    cor.test for all combinations

---
+++ cor.test.2.r
@@ -0,0 +1,15 @@
+## all pairwise correlations done by cor.test()
+## written by tm and pk
+cor.test.2 <- function(x){
+  output <- list()
+  for(i in 1:(length(x)-1))
+    for(j in (i+1):length(x)){
+      output[[names(x)[i]]][[names(x)[j]]] <- cor.test(x[[i]] , x[[j]])
+    }
+  return(unlist(output,recursive=F))
+}
+
+## example
+#cts <- cor.test.2(list(cortex=1:10, insula=1:10 + rnorm(10),hippocampus=1:10) )
+#cts
+#sapply(cts,"[[","p.value")

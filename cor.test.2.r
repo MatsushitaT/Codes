b548549702dc2249a1c1435da77a50a08143fba1
MatsushitaT <MatsushitaT@neurology.ucsf.edu>
2012-07-25 12:25:20 -0700

    Add method

--- cor.test.2.r
+++ cor.test.2.r
@@ -1,15 +1,15 @@
 ## all pairwise correlations done by cor.test()
 ## written by tm and pk
-cor.test.2 <- function(x){
+cor.test.2 <- function(x,meth){
   output <- list()
   for(i in 1:(length(x)-1))
     for(j in (i+1):length(x)){
-      output[[names(x)[i]]][[names(x)[j]]] <- cor.test(x[[i]] , x[[j]])
+      output[[names(x)[i]]][[names(x)[j]]] <- cor.test(x[[i]] , x[[j]],method=meth)
     }
   return(unlist(output,recursive=F))
 }
 
 ## example
-#cts <- cor.test.2(list(cortex=1:10, insula=1:10 + rnorm(10),hippocampus=1:10) )
+#cts <- cor.test.2(list(cortex=1:10, insula=1:10 + rnorm(10),hippocampus=1:10),"spearman" )
 #cts
 #sapply(cts,"[[","p.value")
